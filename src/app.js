9114e2410a1aafbe40eae87656a5b13214335677
RAMMACHyc <sadik.ram12@gmail.com>
2024-03-26 10:07:57 +0000

    Add routes for category, rating, and posts

--- src/app.js
+++ src/app.js
@@ -2,21 +2,29 @@ import "./config"
 import express from "express";
 import routerUser from "./routes/userRoutes";
 import routePlace from "./routes/placeRoutes"
+import routeCategory from "./routes/categoryRoutes"
+import routeRating from "./routes/ratingRoutes"
+import routePost from "./routes/postRoutes"
+import { uploadHandler } from "./middleware/upload";
+import uploadMiddleware from "./util/upload";
 
 const app = express();
 const bodyParser = require('body-parser');
  
+app.use(express.static('public'));
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
-
 const cors = require('cors');
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-}));
+app.use(cors());
 app.use(express.json())
 app.use("/user", routerUser)
 app.use("/place",routePlace)
+app.use("/category",routeCategory)
+app.use("/rating",routeRating)
+app.use("/posts",routePost)
+
+app.post('/upload',uploadMiddleware.single("image"), uploadHandler);
 
 export default app;
 
