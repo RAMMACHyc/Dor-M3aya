f4c78d5f3b4d108525305fa2054577f83f7de235
RAMMACHyc <sadik.ram12@gmail.com>
2024-03-26 10:12:48 +0000

    Add post model schema

---
+++ src/models/post.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        require: true,
+        ref: 'User',
+    },
+    title: { type: String, required: [true, "please"] },
+    file: { type: String, required: [true, "please"] },
+    likes: {
+        type: Number,
+        default: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    }
+})
+
+const post = mongoose.model('Post', postSchema);
+export default post 
