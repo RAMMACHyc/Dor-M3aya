9add7fe4e66663ace77739e7f0dd61a0f0e05568
RAMMACHyc <sadik.ram12@gmail.com>
2024-04-02 22:33:52 +0000

    Add category routes

---
+++ src/routes/categoryRoutes.js
@@ -0,0 +1,12 @@
+import { Router} from "express";
+
+import {categoryController} from '../controllers/categoryController';
+
+const router = Router();
+
+router.get('/', categoryController.getCategories);
+router.post('/', categoryController.createCategory);
+router.put('/:id', categoryController.updateCategory);
+router.delete('/:id', categoryController.deleteCategory);
+
+export default router;
