a5e5e9e97c565b358495765876c8f3fa1e6f713c
RAMMACHyc <sadik.ram12@gmail.com>
2024-04-02 09:26:24 +0000

    create postController.js for create and read places

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,9 +1,19 @@
 
 import Post from "../models/post"
+import User from "../models/userModel"
 import { Types } from "mongoose";
 
 
 export const PostController = {
+  // getPosts: async (req, res) => {
+  //   try {
+  //     const Posts = await User.find().populate('posts');
+  //     res.status(200).json(Posts);
+  //   } catch (error) {
+  //     console.error(error);
+  //     res.status(500).json({ error: 'Internal server error' });
+  //   }
+  // },
 
   getPosts: async (req, res) => {
     try {
@@ -12,37 +22,55 @@ export const PostController = {
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Internal server error' });
-    }
+    } 
   },
-  createPost: async (req, res) => {
+  // createPost: async (req, res) => {
 
+  //   try {
+  //     const { title, file, likes } = req.body;
+  //     console.log(req.body);
+     
+
+  //     if (!(title && file)) {
+  //       return res.status(400).json({ error: 'Please provide all required fields' });
+  //     }
+
+  //     const newPost = new Post({
+  //       title,
+  //       likes,
+  //       file,
+  //     });
+
+  //     const savedPost = await newPost.save();
+
+  //     res.status(201).json({
+  //       message: 'Post created successfully',
+  //       post: savedPost,
+  //     });
+  //   } catch (error) {
+  //     console.error(error);
+  //     res.status(500).json({ error: 'Internal server error' });
+  //   }
+  // },
+
+  createPost: async (req, res) => {
     try {
       const { title, file, likes } = req.body;
       console.log(req.body);
-     
-
       if (!(title && file)) {
         return res.status(400).json({ error: 'Please provide all required fields' });
       }
-
-      const newPost = new Post({
-        title,
-        likes,
-        file,
-      });
-
-      const savedPost = await newPost.save();
-
-      res.status(201).json({
-        message: 'Post created successfully',
-        post: savedPost,
-      });
+      const post = await Post.create(req.body);
+      const userId ='66082032ac6df2a6f3be99dc';
+      console.log(userId);
+      if (userId) {
+        await User.findByIdAndUpdate(userId, { $push: { posts: post._id } });
+      }
+      res.status(201).json(post); 
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal server error' });
+      res.status(400).json({ message: error.message });
     }
   },
-
 
   updatePost: async (req, res) => {
     const { id } = req.params;
