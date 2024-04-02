b2d8615ada3f148500471657b6fc68892dc33e9f
RAMMACHyc <sadik.ram12@gmail.com>
2024-04-02 09:27:06 +0000

    Update category model with new fields

--- src/models/categoryModel.js
+++ src/models/categoryModel.js
@@ -6,11 +6,23 @@ const categorySchema = new mongoose.Schema({
         required: [true, 'Please add a name for the category'],
         unique: true
     },
-    icon: {
+    IconName: {
         type: String,
         required: [true, 'Please add an icon for the category']
-    }
-}, {
+    },
+    color: {
+        type: String,
+        required: [true, 'Please add a color for the category']
+    },
+    
+    places: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Place'
+        }
+  ],  
+
+}, { 
     timestamps: true
 });
 
